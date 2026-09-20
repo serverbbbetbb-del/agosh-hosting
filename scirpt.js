da8887bf1fa5679b9030988de2e1e1400da63ab7
Agoshmc <serverbbbetbb@gmail.com>
2026-09-20 07:15:47 +0500

    Add smooth scrolling for anchor links on page load

---
+++ scirpt.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Sayfa bağlantılarında yumuşak geçiş
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            const target = document.querySelector(this.getAttribute("href"));
+
+            if (target) {
+                target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+
+});
